1084fee6f2fc756e04a022640e3e78476e4322af
sulimanaksir-cell <sulimanaksir@gmail.com>
2025-09-21 20:48:59 +0430

    Add files via upload

---
+++ CircleArea/src/circlearea/CircleArea.java
@@ -0,0 +1,18 @@
+
+package circlearea;
+
+
+public class CircleArea {
+
+    public static void main(String[] args) {
+     
+        double radius ;
+        double area ;
+        
+        radius = 20;
+        area = radius * radius * 3.14159;
+        System.out.println("The area of circle = " + area);
+        
+    }
+    
+}
