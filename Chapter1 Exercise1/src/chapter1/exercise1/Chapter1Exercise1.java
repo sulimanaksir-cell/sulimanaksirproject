e2eb379f42080e3dbce0de30b1bb360bb5fcb980
sulimanaksir-cell <sulimanaksir@gmail.com>
2025-11-16 11:24:18 +0430

    Add files via upload

---
+++ Chapter1 Exercise1/src/chapter1/exercise1/Chapter1Exercise1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter1.exercise1;
+
+/**
+ *
+ * @author Dell
+ */
+public class Chapter1Exercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to java!");
+        System.out.println("Welcome to Computer Science!");
+        System.out.println("Programming is fun!");
+  
+    }
+    
+}
